2fe7b62b0c9466acbfdb55eb19a1c424ae1b7626
pzs19 <64088736+pzs19@users.noreply.github.com>
2021-07-08 23:48:47 +0800

    Delete update directory

--- update/AutoSaveAndTrans.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-int main(){
-    system("bash -c 'source /opt/ros/kinetic/setup.bash;source ~/catkin_ws/devel/setup.bash;"
-    "rosrun waterplus_map_tools wp_saver &&"
-    "cd ~ &&" 
-    "python3 ~/catkin_ws/src/process_waypoints/process_waypoints.py &&"
-    "bash'&");
-    return 0;
-}
